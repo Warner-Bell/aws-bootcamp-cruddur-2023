9b27c6c4d8e030e654f24f1fb8cee32411ada70b
Warner Bell <warner.bell21@gmail.com>
2023-04-19 02:58:03 +0000

    added alter table  wk8 bckend/frflform 42:35

--- backend-flask/db/migrations/1681872761889709_add_bio_column.py
+++ backend-flask/db/migrations/1681872761889709_add_bio_column.py
@@ -3,10 +3,12 @@ from lib.db import db
 class AddBioColumnMigration:
   def migrate_sql():
     data = """
+    ALTER TABLE public.users ADD COLUMN bio text;
     """
     return data
   def rollback_sql():
     data = """
+    ALTER TABLE public.users DROP COLUMN bio;
     """
     return data
 
@@ -17,5 +19,5 @@ class AddBioColumnMigration:
   def rollback():
     db.query_commit(AddBioColumnMigration.rollback_sql(),{
     })
-    
+
 migration = AddBioColumnMigration
