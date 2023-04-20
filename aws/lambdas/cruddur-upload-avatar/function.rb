7cdf970f1b609937e9ee638c154306441492765c
Warner Bell <warner.bell21@gmail.com>
2023-04-20 21:59:45 +0000

    new env

--- aws/lambdas/cruddur-upload-avatar/function.rb
+++ aws/lambdas/cruddur-upload-avatar/function.rb
@@ -10,7 +10,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-warnerbell-awsbootcampc-tg4r201kcvv.ws-us94.gitpod.io",
+        "Access-Control-Allow-Origin": "https://warnerbell-awsbootcampc-i9qhgp63m1k.ws-us95.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200
@@ -38,7 +38,7 @@ def handler(event:, context:)
     { 
       headers: {
         "Access-Control-Allow-Headers": "*, Authorization",
-        "Access-Control-Allow-Origin": "https://3000-warnerbell-awsbootcampc-tg4r201kcvv.ws-us94.gitpod.io",
+        "Access-Control-Allow-Origin": "https://warnerbell-awsbootcampc-i9qhgp63m1k.ws-us95.gitpod.io",
         "Access-Control-Allow-Methods": "OPTIONS,GET,POST"
       },
       statusCode: 200, 
