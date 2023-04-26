0b022202fe6c01803b227e1fb89bc8d52878c500
Warner Bell <100949697+Warner-Bell@users.noreply.github.com>
2023-04-26 08:17:56 -0500

    Update user_activities.py

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -3,19 +3,19 @@ from lib.db import db
 class UserActivities:
   def run(user_handle):
     #try:
-      model = {
-        'errors': None,
-        'data': None
-      }
-      if user_handle == None or len(user_handle) < 1:
-        model['errors'] = ['blank_user_handle']
-      else:
-        sql = db.template('users','show')
-        results = db.query_object_json(sql,{'handle': user_handle})
-        model['data'] = results
-
+    model = {
+      'errors': None,
+      'data': None
+    }
+    if user_handle == None or len(user_handle) < 1:
+      model['errors'] = ['blank_user_handle']
+    else:
+      print("else:")
+      sql = db.template('users','show')
+      results = db.query_object_json(sql,{'handle': user_handle})
+      model['data'] = results
       #subsegment = xray_recorder.begin_subsegment('mock-data')
-      # xray ---
+      ## xray ---
       #dict = {
       #  "now": now.isoformat(),
       #  "results-size": len(model['data'])
@@ -23,6 +23,6 @@ class UserActivities:
       #subsegment.put_metadata('key', dict, 'namespace')
       #xray_recorder.end_subsegment()
     #finally:  
-    #  # Close the segment
+    ##  # Close the segment
     #  xray_recorder.end_subsegment()
-      return model
+    return model
