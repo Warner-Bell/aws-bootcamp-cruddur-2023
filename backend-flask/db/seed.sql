24d025e5d33bd4ef682452ac49ff8fb9a51c4d3a
Warner Bell <warner.bell21@gmail.com>
2023-07-06 19:24:41 +0000

    week x cleanup 30:57

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -11,4 +11,9 @@ VALUES
     (SELECT uuid from public.users WHERE users.handle = 'raymondreddington' LIMIT 1),
     'This was imported as seed data!',
     current_timestamp + interval '10 day'
-  )
+  ),
+  (
+    (SELECT uuid from public.users WHERE users.handle = 'warnerbell' LIMIT 1),
+    'I am the other!',
+    current_timestamp + interval '10 day'
+  );
