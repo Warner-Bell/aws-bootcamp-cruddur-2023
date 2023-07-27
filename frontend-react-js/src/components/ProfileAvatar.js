508b902c156c6c1dca02fd01f46c8270793d0d3c
Warner Bell <warner.bell21@gmail.com>
2023-07-27 22:28:50 +0000

    new deployment tweaks

--- frontend-react-js/src/components/ProfileAvatar.js
+++ frontend-react-js/src/components/ProfileAvatar.js
@@ -1,7 +1,7 @@
 import './ProfileAvatar.css';
 
 export default function ProfileAvatar(props) {
-  const backgroundImage = `url("https://assets.gadgetoolslabs.com/avatars/${props.uuid}.jpg")`;
+  const backgroundImage = `url("https://assets.gadgetoolslabs.com/avatars/${props.id}.jpg")`;
   const styles = {
     backgroundImage: backgroundImage,
     backgroundSize: 'cover',
